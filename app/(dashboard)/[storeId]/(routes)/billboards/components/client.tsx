4ce8f0dd41bbcdf9546ccd747284542498175a3e
Kevin Peng <kpeng66@gmail.com>
2023-07-30 15:20:16 -0700

    finished data table (admin)

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,12 +3,14 @@
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { Billboard } from "@prisma/client"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
 interface BillboardClientProps {
-    data: Billboard[]
+    data: BillboardColumn[]
 }
 
 export const BillboardClient: React.FC<BillboardClientProps> = ({
@@ -29,6 +31,10 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
             </Button>
         </div>
         <Separator />
+        <DataTable columns={columns} data={data} searchKey="label"/>
+        <Heading title="API" description="API calls for Billboards"/>
+        <Separator />
+        <ApiList entityName="billboards" entityIdName="billboardId"/>
         </>
     )
 }
